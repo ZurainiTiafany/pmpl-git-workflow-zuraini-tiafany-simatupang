27c90aec889fd6af02c1aa2a7237f30260be8348
agielstudent <mohammadagiel@student.ub.ac.id>
2026-03-05 10:30:11 +0700

    Commit pertama: Upload Folder

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+// Git Workflow Simulation
+// File ini digunakan untuk simulasi perubahan dalam praktikum Git.
+
+$title = "Git Workflow Simulation";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <h1><?php echo $title; ?></h1>
+
+    <p>Selamat datang di praktikum Git Workflow.</p>
+
+    <button onclick="showMessage()">Klik Saya</button>
+
+    <script src="script.js"></script>
+</body>
+</html>
